d1eebbf97a84a27f788f1c10b907cfd3fa6a17fb
SpaceFox <ujueseo.yeou@gmail.com>
2016-12-20 22:38:08 +0100

    Exercice 1.2

--- src/fr/spacefox/kotlinquarium/Aquarium.kt
+++ src/fr/spacefox/kotlinquarium/Aquarium.kt
@@ -36,11 +36,26 @@ class Aquarium {
 fun main(args: Array<String>) {
     val metalQuarium = Aquarium()
 
-    for (i in 1..4) {
+    for (i in 1..10) {
         metalQuarium.addAlga(Alga())
     }
-    metalQuarium.addFish(Fish("Lemmy Kilmister", Sex.MALE))
-    metalQuarium.addFish(Fish("Floor Jansen", Sex.FEMALE))
+    metalQuarium.addFish(Grouper("Lemmy Kilmister", Sex.MALE))
+    metalQuarium.addFish(Grouper("Floor Jansen", Sex.FEMALE))
+
+    metalQuarium.addFish(Tuna("James Hetfield", Sex.MALE))
+    metalQuarium.addFish(Tuna("Tarja Turunen", Sex.FEMALE))
+
+    metalQuarium.addFish(Clownfish("Dani Filth", Sex.MALE))
+    metalQuarium.addFish(Clownfish("Simone Simons", Sex.FEMALE))
+
+    metalQuarium.addFish(Sole("Ozzy Osbourne", Sex.MALE))
+    metalQuarium.addFish(Sole("Cammie Gilbert", Sex.FEMALE))
+
+    metalQuarium.addFish(Bass("Mikael Åkerfeldt", Sex.MALE))
+    metalQuarium.addFish(Bass("中元 すず香", Sex.FEMALE))
+
+    metalQuarium.addFish(Carp("Joakim Brodén", Sex.MALE))
+    metalQuarium.addFish(Carp("Мари́я Архипова", Sex.FEMALE))
 
     metalQuarium.newTurn()
 }
