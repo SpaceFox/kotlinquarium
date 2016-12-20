d38761242e6d1dde4766746ef45a2c32269d3a8b
SpaceFox <ujueseo.yeou@gmail.com>
2016-12-20 23:26:25 +0100

    Exercice 2.1

--- src/fr/spacefox/kotlinquarium/Aquarium.kt
+++ src/fr/spacefox/kotlinquarium/Aquarium.kt
@@ -1,12 +1,16 @@
 package fr.spacefox.kotlinquarium
 
+import java.util.*
+
 /**
  * Created by spacefox on 20/12/16.
  */
 class Aquarium {
-    val algae = mutableSetOf<Alga>()
-    val fishes = mutableSetOf<Fish>()
-    var age = 0
+    val algae = mutableListOf<Alga>()
+    val fishes = mutableListOf<Fish>()
+
+    private var age = 0
+    private val random = Random()
 
     fun addAlga(alga: Alga) {
         algae.add(alga)
@@ -19,18 +23,46 @@ class Aquarium {
     fun newTurn() {
         age++
 
-        // Pour l'instant on ne fait rien de particulier
+        val survivorFishes: MutableList<Fish> = mutableListOf()
+        survivorFishes.addAll(fishes)
+
+        fishes
+                .asSequence()   // Indispensable pour éviter le traitement de poissons mangés
+                .filter { it in survivorFishes }
+                .forEach {
+                    when (it) {
+                        is Carnivore -> {
+                            val prey = getRandomElement(survivorFishes)
+                            if (prey != null && it.eat(prey)) {
+                                println("$it eats $prey")
+                                survivorFishes.remove(prey)
+                            }
+                        }
+                        is Herbivore -> {
+                            val alga = getRandomElement(algae)
+                            if (alga != null && it.eat(alga)) {
+                                println("$it eats an alga")
+                                algae.remove(alga)
+                            }
+                        }
+                    }
+                }
+
+        fishes.clear()
+        fishes.addAll(survivorFishes)
 
         print(this)
     }
 
+    private fun <E> getRandomElement(list: List<E>): E? {
+        return if (list.isNotEmpty()) list[random.nextInt(list.size)] else null
+    }
+
     override fun toString(): String {
-        var out = "Turn $age. This aquarium contains ${algae.size} algae and ${fishes.size} fishes."
+        var out = "Turn $age. This aquarium contains ${algae.size} algae and ${fishes.size} fishes :"
         fishes.forEach { out += "\n- $it" }
         return out
     }
-
-
 }
 
 fun main(args: Array<String>) {
@@ -57,5 +89,10 @@ fun main(args: Array<String>) {
     metalQuarium.addFish(Carp("Joakim Brodén", Sex.MALE))
     metalQuarium.addFish(Carp("Мари́я Архипова", Sex.FEMALE))
 
-    metalQuarium.newTurn()
+    var input: String?
+    do {
+        metalQuarium.newTurn()
+        println("\nq to quit, <enter> for a new turn.")
+        input = readLine()
+    } while (input?.trim()?.toLowerCase() != "q")
 }
